69df268a62725cf06f9f1af3cbb193af3903a041
ortub10 <ortub10@gmail.com>
2022-12-28 13:56:29 +0200

    creating component counter

--- time_counter/src/App.js
+++ time_counter/src/App.js
@@ -1,16 +1,21 @@
+import React, { Component } from "react";
+import Counter from "./comps/counter";
 import "./App.css";
 
-function App() {
-  let myDate = "2030-01-01";
-  return (
-    <div className="App">
-      <h2>Count down to {myDate}</h2>
-      <h3>Days: XX</h3>
-      {/* It will be in own component */}
-      <input defaultValue="monkeys" />
-      <button className="btn btn-success">change</button>
-    </div>
-  );
+class App extends Component {
+  // myDate = "2030-01-01";
+  render() {
+    return (
+      <div className="App">
+        {/* <h2>Count down to {this.myDate}</h2>
+        <h3>Days: XX</h3> */}
+        {/* It will be in own component */}
+        <Counter />
+        <input defaultValue="monkeys" />
+        <button className="btn btn-success">change</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ time_counter/src/comps/counter.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  myDate = "2030-01-01";
+  render() {
+    return <div>Counter work</div>;
+  }
+}
+
+export default Counter;
